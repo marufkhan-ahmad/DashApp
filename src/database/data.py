8c19d8c7aa61ab283635cbc1a964755e6d5a943a
Maruf khan <2020pgicsmaruf43@poornima.org>
2025-05-22 15:17:07 +0530

    Completed DashApp

--- src/database/data.py
+++ src/database/data.py
@@ -1,31 +1,32 @@
 # src/database/data.py
 
+from dotenv import load_dotenv
+import os
 import pyodbc
 from src.pipeline.exception import CustomException
 
 class Database:
     def __init__(self):
-        # SQL Server connection details
-        self.server = r'LAPTOP-CN6IDNLC\SQLEXPRESS'
-        self.database = 'Flipshope'
+        load_dotenv()
+
+        self.driver = os.getenv("DB_DRIVER")
+        self.server = os.getenv("DB_SERVER")
+        self.database = os.getenv("DB_NAME")
         self.conn_str = (
-            'DRIVER={ODBC Driver 17 for SQL Server};'
+            f'DRIVER={self.driver};'
             f'SERVER={self.server};'
             f'DATABASE={self.database};'
             'Trusted_Connection=yes;'
         )
+
 
     def fetch_data(self):
         try:
             conn = pyodbc.connect(self.conn_str)
             cursor = conn.cursor()
             cursor.execute("SELECT * FROM Hyyzo")
-            data = cursor.fetchall()
             columns = [column[0] for column in cursor.description]
-        
-        # Convert rows to tuples (just in case they are single-element)
-            data = [tuple(row) for row in data]
-
+            data = [list(row) for row in cursor.fetchall()]
             cursor.close()
             conn.close()
             return columns, data
